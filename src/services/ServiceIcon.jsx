2c500227e0f740a491a34f8810f2d2d81a3b9b05
es419 <74262992+es419@users.noreply.github.com>
2026-09-23 00:41:43 +0300

    Use local fitted Mizrahi icon

--- src/services/ServiceIcon.jsx
+++ src/services/ServiceIcon.jsx
@@ -5,7 +5,6 @@ const EMOJIS = ['🔐','🎓','💳','🏦','🛒','🎮','🎬','🎵','✈️'
 
 const SPECIAL_SERVICE_ICONS = {
   'haifa-moodle': 'https://magazine.haifa.ac.il/images/color_logo_three_languages.png',
-  mizrahi: 'https://commons.wikimedia.org/wiki/Special:Redirect/file/%D7%9C%D7%95%D7%92%D7%95%20%D7%A9%D7%9C%20%D7%91%D7%A0%D7%A7%20%D7%9E%D7%96%D7%A8%D7%97%D7%99-%D7%98%D7%A4%D7%97%D7%95%D7%AA.svg'
 };
 export { EMOJIS };
 
